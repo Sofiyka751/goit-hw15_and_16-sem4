d84edc5ebb4ec92dba2757721da2c94da2c71d8f
Sofiyka751 <kolozay.sofiya@gmail.com>
2025-04-17 22:22:30 +0300

    six commit

--- index.js
+++ index.js
@@ -1,147 +1,53 @@
-// import axios from "axios";
-// // axios.get('https://api.example.com/data')
-// //   .then(res => console.log(res.data))
-// //   .catch(err => console.error(err));
-// import debounce from "lodash.debounce";
-// import { alert, info } from "@pnotify/core";
-// import "@pnotify/core/dist/PNotify.css";
-// import "@pnotify/core/dist/BrightTheme.css";
-
-
-
-// const searchInput = document.getElementById('search');
-// const infoList = document.querySelector('.country-list');
-
-// async function getName(valueInput) {
-//     try {
-//         const { data } = await axios.get(`https://restcountries.com/v3.1/name/${valueInput}`);
-        
-//         if (data.length > 10) {
-//             infoList.innerHTML = '';
-//             alert({
-//                 text: "Знайдено занадто багато збігів. Будь ласка, введіть більш конкретний запит!",
-//                 delay: 2000,
-//             });
-//             return;
-//         }
-
-//         makeHtml(data);
-//     } catch (error) {
-//         console.log(error);
-//         alert({
-//             text: "Країна не знайдена. Будь ласка, перевірте ваші дані.",
-//             delay: 2000,
-//         });
-//         infoList.innerHTML = '';
-//     }
-// }
-
-// function makeHtml(countrys) {
-//     const markup = countrys.map(country => {
-//         const population = country.population;
-//         const languages = country.languages
-//             ? Object.values(country.languages).join(', ')
-//             : "Unknown";
-//         return `<li class="country-list-item">
-//                     <p class="country-list-item-name"><strong>${country.name.common}</strong></p>
-//                     <p class="country-list-item-population">Population: ${population}</p>
-//                     <p class="country-list-item-languages">Languages: ${languages}</p>
-//                     <img src="${country.flags.png}" class="country-list-item-flag">
-//                 </li>`;
-//     }).join('');
-//     infoList.innerHTML = markup;
-// }
-
-// function searchCountry(e) {
-//     const value = searchInput.value.trim();
-//     if (value !== '') {
-//         getName(value);
-//     } else {
-//         infoList.innerHTML = '';
-//     }
-// }
-
-// searchInput.addEventListener('input', debounce(searchCountry, 500));
-
-
 const searchInput = document.getElementById('search');
 const infoList = document.querySelector('.country-list');
 
-// Функція для отримання країни за введеним значенням
 async function getName(valueInput) {
-  try {
-    const { data } = await axios.get(`https://restcountries.com/v3.1/name/${valueInput}`);
-    
-    // Якщо знайдено більше 10 результатів, показуємо повідомлення
-    if (data.length > 10) {
-      infoList.innerHTML = '';
-      PNotify.alert({
-        text: "Знайдено занадто багато збігів. Будь ласка, введіть більш конкретний запит!",
-        delay: 2000,
-      });
-      return;
+    try {
+        const { data } = await axios.get(`https://restcountries.com/v3.1/name/${valueInput}`);
+        
+        if (data.length > 10) {
+            infoList.innerHTML = '';
+            alert({
+                text: "Знайдено занадто багато збігів. Будь ласка, введіть більш конкретний запит!",
+                delay: 2000,
+            });
+            return;
+        }
+
+        makeHtml(data);
+    } catch (error) {
+        console.log(error);
+        alert({
+            text: "Країна не знайдена. Будь ласка, перевірте ваші дані.",
+            delay: 2000,
+        });
+        infoList.innerHTML = '';
     }
-
-    // Створюємо HTML для відображення інформації про країни
-    makeHtml(data);
-  } catch (error) {
-    console.log(error);
-    PNotify.alert({
-      text: "Країна не знайдена. Будь ласка, перевірте ваші дані.",
-      delay: 2000,
-    });
-    infoList.innerHTML = '';
-  }
 }
 
-// Функція для створення HTML з даних про країни
 function makeHtml(countrys) {
-  const markup = countrys.map(country => {
-    const population = country.population;
-    const languages = country.languages
-      ? Object.values(country.languages).join(', ')
-      : "Unknown";
-    return `<li class="country-list-item">
-                <p class="country-list-item-name"><strong>${country.name.common}</strong></p>
-                <p class="country-list-item-population">Population: ${population}</p>
-                <p class="country-list-item-languages">Languages: ${languages}</p>
-                <img src="${country.flags.png}" class="country-list-item-flag">
-            </li>`;
-  }).join('');
-  infoList.innerHTML = markup;
+    const markup = countrys.map(country => {
+        const population = country.population;
+        const languages = country.languages
+            ? Object.values(country.languages).join(', ')
+            : "Unknown";
+        return `<li class="country-list-item">
+                    <p class="country-list-item-name"><strong>${country.name.common}</strong></p>
+                    <p class="country-list-item-population">Population: ${population}</p>
+                    <p class="country-list-item-languages">Languages: ${languages}</p>
+                    <img src="${country.flags.png}" class="country-list-item-flag">
+                </li>`;
+    }).join('');
+    infoList.innerHTML = markup;
 }
 
-// Функція для обробки введення в полі пошуку
 function searchCountry(e) {
-  const value = searchInput.value.trim();
-  if (value !== '') {
-    getName(value);
-  } else {
-    infoList.innerHTML = '';
-  }
+    const value = searchInput.value.trim();
+    if (value !== '') {
+        getName(value);
+    } else {
+        infoList.innerHTML = '';
+    }
 }
 
-// Дебаунс для оптимізації пошуку
-const debounceSearch = _.debounce(function(value) {
-  console.log("Шукаємо країну:", value);
-
-  PNotify.alert({
-    text: `Шукаємо країну: ${value}`,
-    type: 'info',
-    delay: 2000
-  });
-
-  axios.get(`https://restcountries.com/v3.1/name/${value}`)
-    .then(function (response) {
-      console.log("Дані країн отримано:", response.data);
-      makeHtml(response.data);
-    })
-    .catch(function (error) {
-      console.error("Помилка при отриманні даних:", error);
-    });
-}, 500);
-
-// Обробник події для введення тексту
-searchInput.addEventListener("input", function(e) {
-  debounceSearch(e.target.value);
-});
+searchInput.addEventListener('input', debounce(searchCountry, 500));
